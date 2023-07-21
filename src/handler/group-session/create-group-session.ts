578da5a967f85b36c7f7ba684e8118bae106851b
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-07-21 23:59:20 +0700

    feat: safe guard handler with middlewares

--- src/handler/group-session/create-group-session.ts
+++ src/handler/group-session/create-group-session.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { buildErr } from "../../contract/base";
 import { ENDPOINTS } from "../endpoints";
 import { IHandler } from "../types";
+import { authMiddleware, mentorCheckMiddleware } from "../../middleware";
 
 export const createGroupSession = async (req: Request, res: Response) => {
   try {
@@ -16,7 +17,10 @@ export const createGroupSessionHandler: IHandler = {
   path: ENDPOINTS.GROUP_SESSION.CREATE_GROUP_SESSION.path,
   method: ENDPOINTS.GROUP_SESSION.CREATE_GROUP_SESSION.method,
   handler: createGroupSession,
-  middlewares: [],
+  middlewares: [
+    authMiddleware,
+    mentorCheckMiddleware,
+  ],
   request: {
     body: {
       content: {},
