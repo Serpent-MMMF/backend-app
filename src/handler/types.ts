17ff1ec41b43c246ec3d6ec05b5dac2514214051
Maria Khelli <13520115@std.stei.itb.ac.id>
2023-07-21 21:15:09 +0700

    feat: add handler types

---
+++ src/handler/types.ts
@@ -0,0 +1,15 @@
+import { RouteConfig } from "@asteasolutions/zod-to-openapi";
+import { RequestHandler } from "express";
+import { REST_METHOD_VALUES } from "../constant";
+import { Prettify } from "../util";
+
+export type IHandler = Prettify<
+  RouteConfig & {
+    method: REST_METHOD_VALUES;
+    handler: RequestHandler;
+    middlewares: RequestHandler[];
+  }
+>;
+
+export type RequestConfig = Pick<RouteConfig, "request">;
+export type ResponseConfig = Pick<RouteConfig, "responses">;
