fa57c0e1ece9cf1fddc3ac93e35a5a513ec1963d
Maria Khelli <13520115@std.stei.itb.ac.id>
2023-07-21 20:38:11 +0700

    fix: naming

--- src/repo/base.ts
+++ src/repo/base.ts
@@ -1,16 +1,11 @@
-import {
-  OptionalGenerated,
-  Model,
-  OptionalNonIdGenerated,
-  OptionalExceptId,
-} from "../model";
+import { Model, OptionalExceptIdGenerated, OptionalGenerated } from "../model";
 import { AllOptional } from "../util";
 
 export interface Repo<T extends Model> {
   create: (data: OptionalGenerated<T>) => Promise<T>;
   findMany: (params?: AllOptional<T>) => Promise<T[]>;
   findOne: (params?: AllOptional<T>) => Promise<T | null>;
-  update: (data: OptionalExceptId<T>) => Promise<T>;
+  update: (data: OptionalExceptIdGenerated<T>) => Promise<T>;
   delete: (id: string) => Promise<T>;
   upsert: (data: OptionalGenerated<T>) => Promise<T>;
 }
