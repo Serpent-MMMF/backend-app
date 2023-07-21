c7eee90696b5f9a170ba2ad553a0a764ac0c5f32
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-07-21 19:42:48 +0700

    feat: setup express

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+const port = 9999;
+
+app.listen(port, () => {
+  console.log(`[Server]: I am running at http://localhost:${port}`);
+});
